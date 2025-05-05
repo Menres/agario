021eac9d38adb3e6a474c5ab108b088e24ff5530
Dmitry <72814729+Menres@users.noreply.github.com>
2025-05-05 08:24:17 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(helmet({
 }));
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
 const playerActivity = new Map();
 const lastChatMessage = new Map();
